e7665eb25d84be0f9be62ef4e5b3ccbd197fa114
Desa <76732650+desaxxx@users.noreply.github.com>
2025-02-04 16:36:24 +0300

    1.0.5

--- src/main/java/org/nandayo/DAPI/Util.java
+++ src/main/java/org/nandayo/DAPI/Util.java
@@ -2,17 +2,16 @@ package org.nandayo.DAPI;
 
 import org.bukkit.Bukkit;
 
-import static org.nandayo.DAPI.HexUtil.color;
-
 @SuppressWarnings("unused")
 public class Util {
 
-    /*
-     * General Util
-     */
-    private final static String PREFIX = "<#857c67>[<#ffd16d>DMentions<#857c67>]&r ";
-
     public static void log(String msg) {
-        Bukkit.getConsoleSender().sendMessage(color(PREFIX + msg));
+        Bukkit.getConsoleSender().sendMessage(HexUtil.color(msg));
+    }
+
+    public static void log(String... msgs) {
+        for (String msg : msgs) {
+            log(msg);
+        }
     }
 }
