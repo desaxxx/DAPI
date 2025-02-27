aac67d68f707bc96b0e67f4ab50c2f03f4292868
Desa <76732650+desaxxx@users.noreply.github.com>
2025-02-27 17:30:50 +0300

    1.0.14

--- src/main/java/org/nandayo/DAPI/DAPI.java
+++ src/main/java/org/nandayo/DAPI/DAPI.java
@@ -1,8 +1,11 @@
 package org.nandayo.DAPI;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 import org.nandayo.DAPI.guimanager.MenuListener;
 
 @SuppressWarnings("unused")
@@ -26,4 +29,22 @@ public final class DAPI {
         }
         Bukkit.getPluginManager().registerEvents(new MenuListener(), plugin);
     }
+
+
+    /**
+     * Set missing arguments message for > ALL < subcommands.
+     * Then you can use {@link org.nandayo.DAPI.command.SubCommand#sendMissingArgsMsg(CommandSender, String, String[], String)} within
+     * subcommands to send this message.
+     * <blockquote><pre>
+     *     Placeholders:
+     *       {command}      - Command name
+     *       {current_args} - Current args entered
+     *       {options}      - Options for the missing args
+     *
+     *     Example missing arguments message: "&cUsage: /{label} {current_args} {options}"
+     * </pre></blockquote>
+     */
+    @Getter
+    @Setter
+    public @NotNull String missingArgsMsg = "";
 }
--- src/main/java/org/nandayo/DAPI/command/SubCommand.java
+++ src/main/java/org/nandayo/DAPI/command/SubCommand.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+import org.nandayo.DAPI.DAPI;
 import org.nandayo.DAPI.HexUtil;
 
 @SuppressWarnings("unused")
@@ -13,28 +14,11 @@ public abstract class SubCommand {
     abstract public boolean onSubCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args);
 
     /**
-     * Set missing arguments message for > ALL < subcommands.
-     * Then you can use {@link #sendMissingArgsMsg(CommandSender, String, String[], String)} within
-     * subcommands to send this message.
-     * <blockquote><pre>
-     *     Placeholders:
-     *       {command}      - Command name
-     *       {current_args} - Current args entered
-     *       {options}      - Options for the missing args
-     *
-     *     Example missing arguments message: "&cUsage: /{label} {current_args} {options}"
-     * </pre></blockquote>
-     */
-    @Getter
-    @Setter
-    public static @NotNull String missingArgsMsg = "";
-
-    /**
      * Sends the missing arguments message.
      * @param sender Command sender
      */
     public void sendMissingArgsMsg(@NotNull CommandSender sender, @NotNull String label, String[] args, String options) {
-        String replacedMsg = missingArgsMsg
+        String replacedMsg = DAPI.getInstance().getMissingArgsMsg()
                 .replace("{label}", label)
                 .replace("{current_args}", String.join(" ", args))
                 .replace("{options}", options);
