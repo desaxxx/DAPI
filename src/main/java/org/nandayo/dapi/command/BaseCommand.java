700e2d7ea3a1c0e56b3228da7d1776ecaf1cce77
Desa <76732650+desaxxx@users.noreply.github.com>
2025-05-03 16:08:38 +0300

    1.1.14

--- src/main/java/org/nandayo/dapi/command/BaseCommand.java
+++ src/main/java/org/nandayo/dapi/command/BaseCommand.java
@@ -9,19 +9,22 @@ import org.nandayo.dapi.Util;
 
 import java.util.List;
 
-@ApiStatus.Experimental
 @SuppressWarnings("unused")
 public abstract class BaseCommand implements CommandExecutor, TabCompleter {
+
+    public <T extends JavaPlugin> BaseCommand(T plugin) {
+        register(plugin);
+    }
 
     abstract @NotNull String command();
     abstract @NotNull String description();
     abstract boolean onCommand(@NotNull CommandSender sender, @NotNull String[] args);
     abstract @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull String[] args);
 
-    public void register(@NotNull JavaPlugin plugin) {
+    public <T extends JavaPlugin> void register(@NotNull T plugin) {
         PluginCommand command = plugin.getCommand(command());
         if (command == null) {
-            Util.log("Command '/" + command() + "' was not found.");
+            Util.log("DAPI: Command '/" + command() + "' was not found.");
             return;
         }
         command.setExecutor(this);
