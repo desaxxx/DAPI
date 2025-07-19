4579a4adda86fe2fec5f2441fb9e6c414f5178dc
desaxxx <0restart001@gmail.com>
2025-07-19 18:51:59 +0300

    1.2.7

--- api/src/main/java/org/nandayo/dapi/DAPI.java
+++ api/src/main/java/org/nandayo/dapi/DAPI.java
@@ -24,7 +24,7 @@ public final class DAPI {
         // no construction
     }
 
-    static public final String VERSION = "1.2.6";
+    static public final String VERSION = "1.2.7";
     static public final String GUI_METADATA_KEY = "DAPI_GUI_" + Util.generateRandomLowerCaseString(8);
 
 
