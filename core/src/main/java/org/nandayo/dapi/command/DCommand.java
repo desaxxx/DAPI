c08493c5da3ec0dade8c85c08f0fecef8015a707
desaxxx <0restart001@gmail.com>
2025-10-30 19:15:35 +0300

    Move onCommand and onTabComplete to DCommand

--- core/src/main/java/org/nandayo/dapi/command/DCommand.java
+++ core/src/main/java/org/nandayo/dapi/command/DCommand.java
@@ -1,16 +1,19 @@
 package org.nandayo.dapi.command;
 
 import org.bukkit.Bukkit;
-import org.bukkit.command.PluginCommand;
+import org.bukkit.command.*;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.nandayo.dapi.util.DAPIException;
+
+import java.util.List;
 
 /**
  * @since 1.4.2
  */
 @ApiStatus.Experimental
-public interface DCommand extends DSubCommand {
+public interface DCommand extends DSubCommand, CommandExecutor, TabCompleter {
 
     @Override
     default @NotNull DCommand command() {
@@ -24,5 +27,27 @@ public interface DCommand extends DSubCommand {
         }
         pluginCommand.setExecutor(this);
         pluginCommand.setTabCompleter(this);
+    }
+
+
+    /**
+     * @since 1.5.1
+     */
+    @Override
+    default boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if(!canExecute(commandSender)) {
+            return true;
+        }
+
+        execute(commandSender, args);
+        return true;
+    }
+
+    /**
+     * @since 1.5.1
+     */
+    @Override
+    default @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        return tabComplete(commandSender, args);
     }
 }
